a3cdf5ae125bf3534e192841070c16ba99c2c54c
Elise Fog <elise@itmustbe.com>
2010-11-11 14:18:05 -0800

    cleanup flex-box demo a bit

--- javascripts/flex-scroll-2.js
+++ javascripts/flex-scroll-2.js
@@ -2,7 +2,7 @@ var Overflow2 = {
     setHeight: function(element) {
         var header_height = $$('header')[0].getHeight();
         var footer_height = $$('footer')[0].getHeight();
-        var total_height = (header_height + footer_height) + 84;
+        var total_height = (header_height + footer_height) + 44;
         var body_height = document.viewport.getHeight() - total_height;
         var max_height = body_height >= 100 ? body_height : 100;
         element.setStyle({
