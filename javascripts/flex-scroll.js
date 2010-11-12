7423ecd7236f32de2576ebf845754bc8eea69e7d
Elise Fog <elise@itmustbe.com>
2010-11-12 11:07:30 -0800

    remove commented out code from flex-scroll

--- javascripts/flex-scroll.js
+++ javascripts/flex-scroll.js
@@ -1,9 +1,5 @@
 var Overflow = {
     setWidth: function(element) {
-        /* var left_width = $('box-2').getWidth();
-        var right_width = $('box-3').getWidth();
-        var total_width = (left_width + right_width) - 75;
-        var body_width = document.viewport.getWidth() - total_width; */
         var body_width = document.viewport.getWidth() - 22;
         var max_width = body_width >= 100 ? body_width : 100;
         element.setStyle({
@@ -11,7 +7,6 @@ var Overflow = {
         });
     },
     setup: function() {
-        /* Overflow.setWidth($('box-1')); */
         Overflow.setWidth($('main'));
         if ($('header')) {
             Overflow.setWidth($('header'));
@@ -27,8 +22,6 @@ var Overflow = {
 document.observe('dom:loaded', function() {
     if (BrowserDetect.browser=="Firefox") {
         Overflow.setup();
-        /* $('box-2').setStyle('min-width: 300px;');
-        $('box-3').setStyle('min-width: 300px;'); */
     }
 });
 Event.observe(window, 'resize', function() {
