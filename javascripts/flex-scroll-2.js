08613ff7ad9b121e26a18e150dd5d421eb0a4df0
Elise Fog <elise@itmustbe.com>
2010-11-11 17:51:16 -0800

    update flex-box demos

--- javascripts/flex-scroll-2.js
+++ javascripts/flex-scroll-2.js
@@ -3,13 +3,17 @@ var Overflow2 = {
         var header_height = $$('header')[0].getHeight();
         var footer_height = $$('footer')[0].getHeight();
         var total_height = (header_height + footer_height) + 44;
+        if ($('extra')) {
+            var extra_height = $('extra').getHeight();
+            var total_height = (total_height + extra_height);
+        }
         var body_height = document.viewport.getHeight() - total_height;
         var max_height = body_height >= 100 ? body_height : 100;
         element.setStyle({
             height: max_height + 'px'
         });
     },
-    setup: function() { /* only works on slideshow listing page but not slideshow add images page in extra div */
+    setup: function() {
         $$('.scroll').each(function(s, index) {
             Overflow2.setHeight(s);
         });
