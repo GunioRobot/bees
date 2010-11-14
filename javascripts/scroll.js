cb58fe58ed25bea6c405aa31497a89da05e386c3
Elise Fog <elise@itmustbe.com>
2010-11-14 10:59:54 -0800

    update almost unused js file for older non flex demo

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -4,18 +4,13 @@ var Overflow = {
         var top_content = $$('#main .content')[0].getHeight();
         var footer_height = $('footer').getHeight();
         var total_height = (header_height + top_content + footer_height) - 75;
-        /* if ($('extra')) {
-            var extra_height = $('extra').getHeight();
-            var total_height = (total_height + extra_height) + 36;
-        }
-        */
         var body_height = document.viewport.getHeight() - total_height;
         var max_height = body_height >= 100 ? body_height : 100;
         element.setStyle({
             height: max_height + 'px'
         });
     },
-    setup: function() { /* only works on slideshow listing page but not slideshow add images page in extra div */
+    setup: function() {
         $$('.scroll').each(function(s, index) {
             Overflow.setHeight(s);
         });
