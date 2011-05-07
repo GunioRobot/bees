1089567923674319280cca01675c6983ebbcd75d
Elise Fog <elise@itmustbe.com>
2011-05-07 15:44:27 -0700

    use beeNamespace with all modules to avoid possible conflicts with other code

--- javascripts/dev/beestyle-toggle-column.js
+++ javascripts/dev/beestyle-toggle-column.js
@@ -1,18 +1,18 @@
 var beeColumn = {
     three: function() {
-        $$('.insert')[0].removeClassName('column2');
-        $$('.insert')[0].addClassName('column3');
+        $$('.insert')[0].removeClassName('beeColumn2');
+        $$('.insert')[0].addClassName('beeColumn3');
         $$('#beeColToggle a')[0].remove();
         $('beeColToggle').insert('<a href="javascript:beeColumn.one();">one column</a>');
     },
     two: function() {
-        $$('.insert')[0].addClassName('column2');
+        $$('.insert')[0].addClassName('beeColumn2');
         $$('#beeColToggle a')[0].remove();
         $('beeColToggle').insert('<a href="javascript:beeColumn.three();">three columns</a>');
     },
     one: function() {
-        $$('.insert')[0].removeClassName('column2');
-        $$('.insert')[0].removeClassName('column3');
+        $$('.insert')[0].removeClassName('beeColumn2');
+        $$('.insert')[0].removeClassName('beeColumn3');
         $$('#beeColToggle a')[0].remove();
         $('beeColToggle').insert('<a href="javascript:beeColumn.two();">two columns</a>');
     }
