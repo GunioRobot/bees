b336ab2f6ec05588e580b801e001ba69b7e66599
Elise <elise@itmustbe.com>
2011-07-18 11:16:54 -0700

    fix hide-show-sidebars to avoid reserved name

--- javascripts/dev-beeflex-options.js
+++ javascripts/dev-beeflex-options.js
@@ -3,9 +3,9 @@ var beeMin = {
         // add min class to main aside
         $(".beeFlex #main aside").addClass("min");
         // wrap panel in a link
-        $(".beeFlex #main aside .panel").wrap("<a href='javascript:beeMin.in();' />");
+        $(".beeFlex #main aside .panel").wrap("<a href='javascript:beeMin.inn();' />");
     },
-    in: function() {
+    inn: function() {
         // remove min class on main aside
         $(".beeFlex #main aside").removeClass("min");
         // unlink panel divs
