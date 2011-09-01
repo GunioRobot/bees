a516382ab114041d640c6159f14da938ba6380da
Elise Fog <elise@itmustbe.com>
2011-09-01 13:02:47 -0700

    update flexslider from v1.5 to v1.6 - now supports multiple sliders on page

--- javascripts/dev-beestyle-flexslider.js
+++ javascripts/dev-beestyle-flexslider.js
@@ -2,7 +2,6 @@ $(window).load(function() {
     $('.flexslider').flexslider({
         animation: "slide",
         slideshow: false,
-        pauseOnHover: true,
         controlsContainer: ".beeSlides"
     });
     $(".beeSlides .next").delay(800).fadeIn("400");
